75a5acfb10cc504b3057f2a7698a04aa7020b298
linsingyu1120-hash <linsingyu1120@gmail.com>
2026-08-08 09:26:12 +0800

    feat: extend led api

--- hardware/src/led.c
+++ hardware/src/led.c
@@ -4,14 +4,56 @@
  */
 #include "led.h"
 
+static led_config led_cfg = {0U};
+
 /* 点亮 LED1 */
-void led_on(void)
+void led_on(uint8_t led_num)
 {
-    HAL_GPIO_WritePin(LED_GPIO_PORT, LED1_PIN, GPIO_PIN_SET);
+    switch(led_num)
+    {
+        case 0U:
+        HAL_GPIO_WritePin(LED_GPIO_PORT, LED1_PIN, GPIO_PIN_SET);
+        break;
+
+        case 1U:
+        HAL_GPIO_WritePin(LED_GPIO_PORT, LED2_PIN, GPIO_PIN_SET);
+        break;
+
+        case 2U:
+        HAL_GPIO_WritePin(LED_GPIO_PORT, LED3_PIN, GPIO_PIN_SET);
+        break;
+
+        case 3U:
+        HAL_GPIO_WritePin(LED_GPIO_PORT, LED4_PIN, GPIO_PIN_SET);
+        break;
+
+        default:
+        break;
+    }
 }
 
 /* 熄灭 LED1 */
-void led_off(void)
+void led_off(uint8_t led_num)
 {
-    HAL_GPIO_WritePin(LED_GPIO_PORT, LED1_PIN, GPIO_PIN_RESET);
+    switch(led_num)
+    {
+        case 0U:
+        HAL_GPIO_WritePin(LED_GPIO_PORT, LED1_PIN, GPIO_PIN_RESET);
+        break;
+
+        case 1U:
+        HAL_GPIO_WritePin(LED_GPIO_PORT, LED2_PIN, GPIO_PIN_RESET);
+        break;
+
+        case 2U:
+        HAL_GPIO_WritePin(LED_GPIO_PORT, LED3_PIN, GPIO_PIN_RESET);
+        break;
+
+        case 3U:
+        HAL_GPIO_WritePin(LED_GPIO_PORT, LED4_PIN, GPIO_PIN_RESET);
+        break;
+
+        default:
+        break;
+    }
 }
